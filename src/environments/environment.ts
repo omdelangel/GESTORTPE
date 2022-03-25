6ce04119250b58a80c1ea5ad10a6abb9d74cef8b
Elena <elenaosiliceo@hotmail.com>
2022-03-25 14:13:43 -0600

    jasg

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   //SERVER_URL: 'http://172.16.8.96:4042',
-  SERVER_URL: 'http://172.16.8.96:4042',
+  SERVER_URL: 'http://172.16.8.90:4042',
   production: true,
   useHash: true,
   hmr: false,
