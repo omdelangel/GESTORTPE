c826753305cb99b3ae8072b588decdd70e5d991a
Elena <elenaosiliceo@hotmail.com>
2022-03-31 18:52:02 -0600

    cambios Elena

--- src/app/_components/inicio/inicio.component.ts
+++ src/app/_components/inicio/inicio.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
- 
+  dataPerfil: any = "";
+  perfil: number = 0;
+  viewDashboard: boolean = false;
+  viewBienvenidos: boolean = false;
 
   constructor() { 
+
+     //Obtiene el valor del perfil para la revisión de los documentos
+     this.dataPerfil = sessionStorage.getItem('usuario');
+     let valores = JSON.parse(this.dataPerfil);
+     this.perfil = valores.IdPerfil;
+
+     if (this.perfil == 6) {
+
+      this.viewDashboard = false;
+      this.viewBienvenidos = true;
+ 
+    } else {
+
+      this.viewDashboard = true;
+      this.viewBienvenidos = false;
+ 
+    }
 
   }
 
